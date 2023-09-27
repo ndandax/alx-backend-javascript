1d52242fae9ef87370cd2e5b49a7023722e487d7
ndandax <sebandandala99@gmail.com>
2023-09-27 22:12:24 +0300

    es6-promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,12 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const promises = [uploadPhoto(), createUser()];
-  
+
   return Promise.all(promises)
     .then((results) => {
       const [uploadPhotoResult, createUserResult] = results;
       return `${uploadPhotoResult.body} ${createUserResult.firstName} ${createUserResult.lastName}`;
     })
-    .catch(() => {
-      return 'Signup system offline';
-    });
+    .catch(() => 'Signup system offline');
 }
