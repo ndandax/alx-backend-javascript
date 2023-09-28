b0ce0a54cdb891be1eb81bbd9efbdb5e4c02b6b6
ndandax <sebandandala99@gmail.com>
2023-09-28 12:13:47 +0300

    es6-classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -12,7 +12,7 @@ export default class HolbertonCourse {
       throw new TypeError('Length must be a number');
     }
 
-    if (typeof students === 'object') {
+    if (Array.isArray(value) && value.every((student) => typeof student === 'string')) {
       this._students = students;
     } else {
       throw new TypeError('Students must be an array of strings');
@@ -44,7 +44,7 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    if (typeof value === 'object') {
+    if (Array.isArray(value) && value.every((student) => typeof student === 'string')) {
       this._students = value;
     }
   }
